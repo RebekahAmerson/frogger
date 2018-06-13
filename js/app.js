e2652015d814147eb648cb99ed797c9214d8573e
Rebekah Amerson <becka_811@hotmail.com>
2018-06-13 14:50:34 -0500

    Create Enemy and Player classes

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// var Enemy = function() {
+//     // Variables applied to each of our instances go here,
+//     // we've provided one for you to get started
+//
+//     // The image/sprite for our enemies, this uses
+//     // a helper we've provided to easily load images
+//     this.sprite = 'images/enemy-bug.png';
+// };
+//
+// // Update the enemy's position, required method for game
+// // Parameter: dt, a time delta between ticks
+// Enemy.prototype.update = function(dt) {
+//     // You should multiply any movement by the dt parameter
+//     // which will ensure the game runs at the same speed for
+//     // all computers.
+// };
+//
+// // Draw the enemy on the screen, required method for game
+// Enemy.prototype.render = function() {
+//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// };
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -44,3 +44,39 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
+//Start my code. TODO reorgainize later.
+
+class Enemy {
+  constructor () {
+    this.sprite = 'images/enemy-bug.png';
+  }
+
+  update(dt) {
+    console.log("update");
+  }
+
+  render() {
+    console.log('render');
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+}
+
+class Player {
+  constructor () {
+    this.sprite = 'images/char-boy.png';
+  }
+
+  update(dt) {
+    console.log("update");
+  }
+
+  render() {
+    console.log('render');
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+}
+
+let allEnemies = [];
+let player = new Player;
